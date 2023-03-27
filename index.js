ce2f95390c21ce06a3df46663e2dc799f0dff797
Svetlana Govyazina <govyazina@gmail.com>
2023-03-27 19:15:38 +0300

    task 1 added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // 1
 // Заполните массив следующим образом: в первый элемент запишите '1',
 //     во второй '22', в третий '333' и так далее.
+
+const task1 = (end) => {
+    const arr = []
+    for (let i = 1; i <= end; i++) {
+        const k = String(i)
+        arr.push(k.repeat(i))
+    }
+    return arr
+}
+
+console.log('задача 1: ' + task1(5))
