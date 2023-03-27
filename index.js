fcf40051216013cf0432de891c8db67c3a369486
Svetlana Govyazina <govyazina@gmail.com>
2023-03-27 19:46:23 +0300

    task 3 added

--- index.js
+++ index.js
@@ -28,3 +28,16 @@ const task2 = (arr) => {
 }
 
 console.log('задача 2: ' + task2([2,2,4,5,5,5]))
+
+// 3
+// Напишите функцию, которая очищает массив от нежелательных значений,
+//     таких как false, undefined, пустые строки, ноль, null.
+//     Ожидаемый результат:
+//     [0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+
+const task3 = (arr) => {
+   return arr.filter(el => el)
+}
+
+console.log('задача 3: ' + task3([0, 1, false, 2, undefined, '', 3, null]))
+
